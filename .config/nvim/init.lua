90b117ecad9c2668231c05c08c9d89be8a9d7aca
Samuel Mannehed <samuel@cendio.se>
2023-05-08 13:19:20 +0200

    Move breadcrumb code to separate "breadcrumbs.lua"

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -461,37 +461,40 @@ require("lazy").setup({
   -- Statusline
   { "nvim-lualine/lualine.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filetype', {'filename', path = 1}},
-        lualine_x = {'searchcount'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {{'filename', path = 1}},
-        lualine_x = {},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-      tabline = {},
-      winbar = {},
-      -- winbar buggy with fugitive
-      -- (see https://github.com/nvim-lualine/lualine.nvim/issues/948)
-      -- winbar = {
-      --   lualine_a = {},
-      --   lualine_b = {},
-      --   lualine_c = {},
-      --   lualine_x = {'filetype'},
-      --   lualine_y = {'filename'},
-      --   lualine_z = {breadcrumbs}
-      -- },
-      inactive_winbar = {},
-    },
+    config = function()
+      -- require("breadcrumbs")
+      require("lualine").setup({
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filetype', {'filename', path = 1}},
+          lualine_x = {'searchcount'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {{'filename', path = 1}},
+          lualine_x = {},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        tabline = {},
+        winbar = {},
+        -- winbar buggy with fugitive
+        -- (see https://github.com/nvim-lualine/lualine.nvim/issues/948)
+        -- winbar = {
+        --   lualine_a = {},
+        --   lualine_b = {},
+        --   lualine_c = {},
+        --   lualine_x = {'filetype'},
+        --   lualine_y = {'filename'},
+        --   lualine_z = {Breadcrumbs}
+        -- },
+        inactive_winbar = {},
+      })
+    end
   },
 
   -- Nice colorscheme (no proper highligts for Python)
@@ -749,46 +752,3 @@ require("lazy").setup({
     end,
   },
 })
-
----- breadcrumbs in lualine ------
-
--- onedark 'deep' bg1 & orange
-api.nvim_set_hl(0, 'bc1_s', { fg = '#abb2bf', bg = '#21283b' })
-api.nvim_set_hl(0, 'bc1_m', { fg = '#dd9046', bg = '#21283b' })
-api.nvim_set_hl(0, 'bc1_e', { fg = '#1a212e', bg = '#21283b' })
-
--- onedark 'deep' bg0 & blue
-api.nvim_set_hl(0, 'bc2_s', { fg = '#abb2bf', bg = '#1a212e' })
-api.nvim_set_hl(0, 'bc2_m', { fg = '#61afef', bg = '#1a212e' })
-api.nvim_set_hl(0, 'bc2_e', { fg = '#1a212e', bg = '#1a212e' })
-
--- onedark 'deep' bg0 & cyan
-api.nvim_set_hl(0, 'bc3_s', { fg = '#abb2bf', bg = '#1a212e' })
-api.nvim_set_hl(0, 'bc3_m', { fg = '#56b6c2', bg = '#1a212e' })
-api.nvim_set_hl(0, 'bc3_e', { fg = '#1a212e', bg = '#1a212e' })
-
-local symbols = {
-  { 'bc1_s', '  ', 'bc1_m', 'bc1_e', '' },
-  { 'bc2_s', ' 󰊕 ', 'bc2_m', 'bc2_e', ' %##%#bc2_e#' },
-  { 'bc3_s', '  ', 'bc3_m', 'bc3_e', '%##%#bc3_e#' },
-}
-
--- show breadcrumbs if available
-local function breadcrumbs()
-  local items = vim.b.coc_nav
-  local t = {''}
-  for k,v in ipairs(items) do
-    setmetatable(v, { __index = function(table, key)
-      return ''
-    end})
-    t[#t+1] = '%#' .. (symbols[k][1] or 'Normal') .. '#' ..
-              (symbols[k][2] or '') ..
-              '%#' .. (symbols[k][3] or 'Normal') .. '#' ..
-              (v.name or '')
-    if next(items,k) ~= nil then
-      t[#t+1] = '%#' .. (symbols[k][4] or 'Normal') .. '#' ..
-                (symbols[k][5] or '')
-    end
-  end
-  return table.concat(t)
-end
---
+++ .config/nvim/lua/breadcrumbs.lua
@@ -0,0 +1,44 @@
+local api = vim.api
+
+---- breadcrumbs in lualine ------
+
+-- onedark 'deep' bg1 & orange
+api.nvim_set_hl(0, 'bc1_s', { fg = '#abb2bf', bg = '#21283b' })
+api.nvim_set_hl(0, 'bc1_m', { fg = '#dd9046', bg = '#21283b' })
+api.nvim_set_hl(0, 'bc1_e', { fg = '#1a212e', bg = '#21283b' })
+
+-- onedark 'deep' bg0 & blue
+api.nvim_set_hl(0, 'bc2_s', { fg = '#abb2bf', bg = '#1a212e' })
+api.nvim_set_hl(0, 'bc2_m', { fg = '#61afef', bg = '#1a212e' })
+api.nvim_set_hl(0, 'bc2_e', { fg = '#1a212e', bg = '#1a212e' })
+
+-- onedark 'deep' bg0 & cyan
+api.nvim_set_hl(0, 'bc3_s', { fg = '#abb2bf', bg = '#1a212e' })
+api.nvim_set_hl(0, 'bc3_m', { fg = '#56b6c2', bg = '#1a212e' })
+api.nvim_set_hl(0, 'bc3_e', { fg = '#1a212e', bg = '#1a212e' })
+
+local symbols = {
+  { 'bc1_s', '  ', 'bc1_m', 'bc1_e', '' },
+  { 'bc2_s', ' 󰊕 ', 'bc2_m', 'bc2_e', ' %##%#bc2_e#' },
+  { 'bc3_s', '  ', 'bc3_m', 'bc3_e', '%##%#bc3_e#' },
+}
+
+-- show breadcrumbs if available
+function Breadcrumbs()
+  local items = vim.b.coc_nav
+  local t = {''}
+  for k,v in ipairs(items) do
+    setmetatable(v, { __index = function(table, key)
+      return ''
+    end})
+    t[#t+1] = '%#' .. (symbols[k][1] or 'Normal') .. '#' ..
+              (symbols[k][2] or '') ..
+              '%#' .. (symbols[k][3] or 'Normal') .. '#' ..
+              (v.name or '')
+    if next(items,k) ~= nil then
+      t[#t+1] = '%#' .. (symbols[k][4] or 'Normal') .. '#' ..
+                (symbols[k][5] or '')
+    end
+  end
+  return table.concat(t)
+end
