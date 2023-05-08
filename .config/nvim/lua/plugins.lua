671a43d8f300b06ac8e74c0e121a1ccadfbd8d94
Samuel Mannehed <samuel@cendio.se>
2023-05-08 17:24:30 +0200

    Move Noice setup to separate file in "setup/"

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -242,21 +242,7 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-        presets = {
-          long_message_to_split = true,
-          command_palette = true,
-        },
-      })
-    end,
+    config = require("setup.noice"),
   },
 
   -- keybinding helper
