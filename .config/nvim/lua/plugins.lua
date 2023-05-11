d5cbaddeecf429660b1dcc403952cdd8be8eed47
Samuel Mannehed <samuel@cendio.se>
2023-05-11 09:11:39 +0200

    Remove defunct plugin tsnode-marker.nvim
    The autocommand doesn't work since it depends on treesitter which might
    load after the autocommand. The benefit from this plugin was was
    questionable.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -161,21 +161,4 @@ return {
   { "folke/which-key.nvim",
     config = require("setup.whichkey"),
   },
-
-  -- highlight context changes in files
-  { "atusy/tsnode-marker.nvim",
-    lazy = true,
-    init = function()
-      api.nvim_create_autocmd("FileType", {
-        group = api.nvim_create_augroup("tsnode-marker-markdown", {}),
-        pattern = "markdown",
-        callback = function(ctx)
-          require("tsnode-marker").set_automark(ctx.buf, {
-            target = { "code_fence_content" }, -- list of target node types
-            hl_group = "CursorLine", -- highlight group
-          })
-        end,
-      })
-    end,
-  },
 }
