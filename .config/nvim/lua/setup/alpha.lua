b6e27ea0a49b3511a61f35c5169c47126b6f044b
Samuel Mannehed <samuel@cendio.se>
2023-05-12 01:15:40 +0200

    Use a pretty string for git overview shortcut

--- .config/nvim/lua/setup/alpha.lua
+++ .config/nvim/lua/setup/alpha.lua
@@ -6,7 +6,7 @@ return function()
     dashboard.button("SPC f f", "󰈞  Find file"),
     dashboard.button("SPC f r", "  Recent files"),
     dashboard.button("SPC g", "󰈬  Live grep"),
-    dashboard.button("<C-g>", "  Show git overview"),
+    dashboard.button("CTRL + g", "  Show git overview", "<C-g>"),
     dashboard.button("q", "  Quit NVIM" , ":qa<CR>"),
   }
   alpha.setup(dashboard.config)
