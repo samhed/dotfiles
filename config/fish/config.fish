d8a89dc9ca624ee1290de86821cc3f9ae4f7ccf8
Samuel Mannehed <samuel@cendio.se>
2023-05-26 00:49:29 +0200

    Migrate aliases and git rshow from bash to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,7 @@
+####################
+# Theme
+####################
+
 set -g theme_display_git_default_branch yes
 set -g theme_date_format "+%H:%M:%S"
 set -g theme_date_timezone Sweden/Stockholm
@@ -7,6 +11,50 @@ set -g theme_color_scheme dark
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '$ '
 set -g default_user no
+
+####################
+# Utility functions
+####################
+
+function git
+    switch $argv[1]
+        case "rshow"
+            if count $argv < 2
+                echo "Usage: git rshow <revision>"
+                return 1
+            end
+            set -l svnrev $argv[2]
+
+            if not string match 'r*' $svnrev
+                set -l svnrev (string join 'r' $svnrev)
+            end
+
+            set -l githash (git svn find-rev $svnrev)
+
+            command /usr/bin/git show $githash $argv[3..-1]
+        case "*"
+            command /usr/bin/git $argv[1..-1]
+    end
+    return $status
+end
+
+####################
+# Aliases
+####################
+
+alias vim="nvim"
+
+alias vecka='date +%V'
+alias diskspace="du -S | sort -n -r |more"
+
+alias sudo='sudo '
+
+alias untar='tar -xvf'
+
+alias cdc='cd ~/devel/ctc-git'
+
+# git diff for any diff
+alias diff="git diff --no-index"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
