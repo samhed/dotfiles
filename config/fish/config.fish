214cc58a6fcc05b8b555fab97df88267af755308
Samuel Mannehed <samuel@cendio.se>
2023-05-31 11:21:30 +0200

    Stop setting environment variables in config.fish
    Since we now start fish from .bashrc we inherit the same environment
    variables as those set in the beginning of .bashrc.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,4 @@
 ####################
-# Environment vars
-####################
-
-set -x PATH $PATH /home/samuel/bin/ /home/samuel/.local/bin/ /opt/cendio-build/bin /opt/thinlinc/bin /opt/thinlinc/sbin
-set -x SVN_EDITOR nvim
-set -x EDITOR nvim
-set -x VISUAL nvim
-set -x PAGER less
-
-####################
 # Theme
 ####################
 
