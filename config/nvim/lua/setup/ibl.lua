8031437d6d8559a548fbbf33706be5d5fb8d41bd
Samuel Mannehed <samuel@cendio.se>
2023-10-08 00:54:29 +0200

    Show very faint indent guides for all scopes
    Previous commit only showed indent guides for the active scope. This was
    a bit harsh. This shows an extremely faint guide.

--- config/nvim/lua/setup/ibl.lua
+++ config/nvim/lua/setup/ibl.lua
@@ -1,17 +1,22 @@
 return function()
+  local hooks = require("ibl.hooks")
+  hooks.register(
+    hooks.type.WHITESPACE,
+    hooks.builtin.hide_first_space_indent_level
+  )
+  hooks.register(
+    hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "NonScopeGuide", { fg = "#1f2634" })
+    end
+  )
+
   require("ibl").setup({
     indent = {
-      highlight = { "Ignore" },
+      highlight = { "NonScopeGuide" },
     },
     scope = {
       show_start = false,
       show_end = false,
     },
   })
-
-  local hooks = require("ibl.hooks")
-  hooks.register(
-    hooks.type.WHITESPACE,
-    hooks.builtin.hide_first_space_indent_level
-  )
 end
