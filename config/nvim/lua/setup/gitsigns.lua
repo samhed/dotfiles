54a21fd509dd5431c454788131ef483159d77186
Samuel Mannehed <samuel@cendio.se>
2024-05-16 11:16:59 +0200

    Add keymaps for gitsigns to jump between changes

--- config/nvim/lua/setup/gitsigns.lua
+++ config/nvim/lua/setup/gitsigns.lua
@@ -7,6 +7,30 @@ return function()
       topdelete    = { text = "╿" },
       changedelete = { text = "┝" },
       untracked    = { text = "┊" },
-    }
+    },
+    on_attach = function(bufnr)
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ',', function()
+        if vim.wo.diff then
+          vim.cmd.normal({',', bang = true})
+        else
+          require("gitsigns").nav_hunk('next')
+        end
+      end)
+
+      map('n', 'm', function()
+        if vim.wo.diff then
+          vim.cmd.normal({'m', bang = true})
+        else
+          require("gitsigns").nav_hunk('prev')
+        end
+      end)
+    end,
   })
 end
