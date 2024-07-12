6b5acfffdac6724c54dcc6638d74ee94c3c008b8
Samuel Mannehed <samuel@cendio.se>
2024-07-12 16:54:10 +0200

    Fix codeium keybindings
    It seems codeium has some bug where using nvim_set_keymap is broken.
    Lets use what their example documentation recommends.

--- config/nvim/lua/setup/codeium.lua
+++ config/nvim/lua/setup/codeium.lua
@@ -1,17 +1,19 @@
 return function()
-  -- <Ctrl+SPACE> --> Accept the Codeium suggestion
   vim.g.codeium_disable_bindings = 1
-  -- vim.api.nvim_set_keymap('i', '<C-space>', 'codeium#Accept("<CR>")',
-  vim.api.nvim_set_keymap('i', '<C-space>', 'codeium#Accept()',
-                          { silent = true, expr = true,
-                            desc = 'Accept Codeium suggestion' })
   -- vim.api.nvim_set_keymap('i', '<M-;>', 'codeium#CycleCompletions("1")',
   --                         { silent = true, expr = true,
   --                           desc = 'Next Codeium suggestion' })
   -- vim.api.nvim_set_keymap('i', '<M-,>', 'codeium#CycleCompletions("-1")',
   --                         { silent = true, expr = true,
   --                           desc = 'Previous Codeium suggestion' })
-  vim.api.nvim_set_keymap('i', '<C-x>', 'codeium#Clear()',
-                          { silent = true, expr = true,
-                            desc = 'Clear Codeium suggestion' })
+
+  -- <Ctrl+SPACE> --> Accept the Codeium suggestion
+  vim.keymap.set('i', '<C-space>', function () return vim.fn['codeium#Accept']() end,
+                 { silent = true, expr = true,
+                   desc = 'Accept Codeium suggestion' })
+
+  -- <Ctrl+x> --> Clear Codeium suggestion
+  vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end,
+                 { silent = true, expr = true,
+                   desc = 'Clear Codeium suggestion' })
 end
