b1de67bcfa10e76728a4e12b15150d1ecbc360ab
Samuel Mannehed <samuel@cendio.se>
2024-07-12 16:54:53 +0200

    Enable codeium keybinds for next/prev completion
    It works well when using vim.keymap.set instead of nvim_set_keymap.

--- config/nvim/lua/setup/codeium.lua
+++ config/nvim/lua/setup/codeium.lua
@@ -1,16 +1,18 @@
 return function()
   vim.g.codeium_disable_bindings = 1
-  -- vim.api.nvim_set_keymap('i', '<M-;>', 'codeium#CycleCompletions("1")',
-  --                         { silent = true, expr = true,
-  --                           desc = 'Next Codeium suggestion' })
-  -- vim.api.nvim_set_keymap('i', '<M-,>', 'codeium#CycleCompletions("-1")',
-  --                         { silent = true, expr = true,
-  --                           desc = 'Previous Codeium suggestion' })
 
   -- <Ctrl+SPACE> --> Accept the Codeium suggestion
   vim.keymap.set('i', '<C-space>', function () return vim.fn['codeium#Accept']() end,
                  { silent = true, expr = true,
                    desc = 'Accept Codeium suggestion' })
+
+  -- <Ctrl+Down> and <Ctrl+Up> --> Cycle through Codeium suggestions
+  vim.keymap.set('i', '<c-Down>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+                 { silent = true, expr = true,
+                   desc = 'Next Codeium suggestion' })
+  vim.keymap.set('i', '<c-Up>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+                 { silent = true, expr = true,
+                   desc = 'Previous Codeium suggestion' })
 
   -- <Ctrl+x> --> Clear Codeium suggestion
   vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end,
