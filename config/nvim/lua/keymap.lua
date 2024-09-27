a27ccc778e1e8350f4389d66dc9bbdda22326b5f
Samuel Mannehed <samuel@cendio.se>
2024-09-27 17:13:48 +0200

    Add w!! keymap to write file that needs sudo

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -37,6 +37,67 @@ keyset('v', '<A-k>', ':m .-2<CR>gv=gv', optsUp)
 keyset('v', '<A-Up>', ':m .-2<CR>gv=gv', optsUp)
 
 ------------------------------------------
+-- Sudo
+------------------------------------------
+
+-- Show notifications for sudo stuff
+local function sudo_notify(cmd, text)
+  local notify_opts = {
+    title = text,
+    icon = ' ',
+    render = 'compact',
+    hide_from_history = true,
+    timeout = 1,
+  }
+  vim.notify(cmd, 'warn', notify_opts)
+end
+
+local function sudo_exec(cmd, print_output)
+  vim.fn.inputsave()
+  local password = vim.fn.inputsecret("Password: ")
+  vim.fn.inputrestore()
+  if not password or #password == 0 then
+    sudo_notify("Invalid password, sudo aborted")
+    return false
+  end
+  local out = vim.fn.system(string.format("sudo -p '' -S %s", cmd), password)
+  if vim.v.shell_error ~= 0 then
+    print("\r\n")
+    sudo_notify(out)
+    return false
+  end
+  if print_output then print("\r\n", out) end
+  return true
+end
+
+local function sudo_write(tmpfile, filepath)
+  if not tmpfile then tmpfile = vim.fn.tempname() end
+  if not filepath then filepath = vim.fn.expand("%") end
+  if not filepath or #filepath == 0 then
+    sudo_notify("E32: No file name")
+    return
+  end
+  -- `bs=1048576` is equivalent to `bs=1M` for GNU dd or `bs=1m` for BSD dd
+  -- Both `bs=1M` and `bs=1m` are non-POSIX
+  local cmd = string.format("dd if=%s of=%s bs=1048576",
+    vim.fn.shellescape(tmpfile),
+    vim.fn.shellescape(filepath))
+  -- no need to check error as this fails the entire function
+  vim.api.nvim_exec2(string.format("write! %s", tmpfile), { output = true })
+  if sudo_exec(cmd) then
+    -- refreshes the buffer and prints the "written" message
+    vim.cmd.checktime()
+    -- exit command mode
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(
+      "<Esc>", true, false, true), "n", true)
+  end
+  vim.fn.delete(tmpfile)
+end
+
+local sudoOpts = { silent = true, desc = 'Write with sudo' }
+keyset('n', 'w!!', sudo_write, sudoOpts)
+
+------------------------------------------
 -- Lazy
 ------------------------------------------
 
